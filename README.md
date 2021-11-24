d0d914db581a0554b5b383126d503cda3017f35c
Naveen Malik <nmalik@redhat.com>
2021-11-24 16:41:30 -0500

    Initial implementation
    Seems to work though RMS could use some tuning.  I think I'll need to read the source for PHD2 to see how many samples it uses and exactly how total RMS is calculated.  I've used what I think is a reasonable solution and it is close enough.

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # phd2-exporter
 Uses PHD2 event monitoring to export metrics.
+
+# Setup
+
+Nothing to do, just fire up PHD2.  It serves traffic on port 4400.  Subsequent instances are on a 4401, 4402, etc.
+
+## Usage
+
+Simply run the exporter with a port and the config you've created / edited
+
+```shell
+python src\phd2-exporter.py --port 8012
+```
+
+Optional arguments are document in `--help`.
+
+## Verify
+
+In your favorite browser look at the metrics endpoint.  If it's local, you can use http://localhost:8012
---
+++ src/phd2-exporter.py
@@ -0,0 +1,290 @@
+import argparse
+import socket
+import json
+import time
+import copy
+import math
+import random
+
+import httpimport
+
+with httpimport.github_repo('jewzaam', 'metrics-utility', 'utility', 'main'):
+    import utility
+
+PHD_STATE = ""
+PHD_RMS_SAMPLES = 15
+PHD_RMS_DATA = {}
+PIXEL_SCALE = 0
+JRPC_CALLBACKS = {}
+
+GLOBAL_LABELS = {}
+
+DEBUG = True
+
+def debug(message):
+    # simply so I can easily control debug messages
+    if DEBUG:
+        print(message)
+
+def callback_requestPixelScale(data):
+    global GLOBAL_LABELS, PIXEL_SCALE
+    PIXEL_SCALE = data["result"]
+    utility.set("phd2_pixel_scale", PIXEL_SCALE, GLOBAL_LABELS)
+
+def requestPixelScale(s):
+    global JRPC_CALLBACKS
+    # socket exception handling done in main loop
+    # send request
+    request_id = random.randint(1,50000)
+    request = {
+        "method": "get_pixel_scale",
+        "id": request_id,
+    }
+    JRPC_CALLBACKS[request_id] = callback_requestPixelScale
+    s.sendall((json.dumps(request) + "\r\n").encode('utf-8'))
+            
+def makeLabels(data, label_keys):
+    labels = {}
+
+    for label_key in label_keys:
+        if label_key in data:
+            labels[label_key] = data[label_key]
+    
+    return labels
+
+def createEventMetrics(data, labels, metric_keys):
+    for metric_key in metric_keys:
+        if metric_key in data:
+            value = data[metric_key]
+            if isinstance(value, bool):
+                if value == True:
+                    value = 1
+                else:
+                    value = 0
+            utility.set(f"phd2_{data['Event']}_{metric_key}", data[metric_key], labels)
+
+def handleEvent(s, data):
+    global PHD_STATE, PHD_RMS_SAMPLES, GLOBAL_LABELS, PHD_RMS_DATA
+
+    if 'Event' not in data:
+        return
+    
+    event = data['Event']
+
+    # all possible states https://github.com/OpenPHDGuiding/phd2/wiki/EventMonitoring#appstate
+    app_states = [
+        "Stopped",      # PHD is idle
+        "Selected",     # A star is selected but PHD is neither looping exposures, calibrating, or guiding
+        "Calibrating",  # PHD is calibrating
+        "Guiding",      # PHD is guiding
+        "LostLock",     # PHD is guiding, but the frame was dropped
+        "Paused",       # PHD is paused
+        "Looping",      # PHD is looping exposures
+        # additional states
+        "Settling",     # PHD is settling
+    ]
+    # use global PHD_STATE to get an updated state, which is then updated at the end of the function
+    
+    if event in ["Version", "AppState"]:
+        # initialze global labels
+        GLOBAL_LABELS = {
+            "host": data["Host"].lower(),
+            "instance": data["Inst"],
+        }
+
+    # update state based on event
+    if event in ["LoopingExposuresStopped"]:
+        PHD_STATE="Stopped"
+    elif event in ["LockPositionSet"]:
+        PHD_STATE="Selected"
+    elif event in ["StartCalibration", "Calibrating"]:
+        PHD_STATE="Calibrating"
+    elif event in ["GuideStep", "SettleDone"]:
+        PHD_STATE="Guiding"
+    elif event in ["StarLost"]:
+        PHD_STATE="LostLock"
+    elif event in ["Paused"]:
+        PHD_STATE="Paused"
+    elif event in ["LoopingExposures"]:
+        PHD_STATE="Looping"
+    elif event in ["SettleBegin", "Settling"]:
+        PHD_STATE="Settling"
+    elif event == "AppState" and "State" in data:
+        PHD_STATE = data["State"]
+
+    for state in app_states:
+        value = 0
+        if state == PHD_STATE:
+            value = 1
+        l = copy.deepcopy(GLOBAL_LABELS)
+        l.update({'status': state})
+        utility.set("phd2_status", value, l)
+
+    # did equipment change? get pixel scale!
+    if event in ["AppState", "ConfigurationChange"]:
+        requestPixelScale(s)
+
+    if event in ["LockPositionSet", "StarSelected"]:
+        createEventMetrics(data, GLOBAL_LABELS, ["X", "Y"])
+    elif event == "Calibrating":
+        l=copy.deepcopy(GLOBAL_LABELS)
+        l.update(makeLabels(data, ["dir"]))
+        createEventMetrics(data, l, ["dist", "dx", "dy", "step"])
+    elif event == "Settling":
+        createEventMetrics(data, GLOBAL_LABELS, ["Distance", "Time", "SettleTime", "StarLocked"])
+    elif event == "SettleDone":
+        # TODO consider if should flip "Status" from 0==success to 1==success....
+        createEventMetrics(data, GLOBAL_LABELS, ["Status", "TotalFrames", "DroppedFrames"])
+    elif event == "StarLost":
+        createEventMetrics(data, GLOBAL_LABELS, ["StarMass", "SNR", "AvgDist", "ErrorCode"])
+    elif event == "GuideStep":
+        # general metrics
+        createEventMetrics(data, GLOBAL_LABELS, ["dx", "dy", "RADistanceRaw", "DECDistanceRaw", "RADistanceGuide", "DECDistanceGuide", "StarMass", "SNR", "HFD", "AvgDist", "ErrorCode"])
+
+        # RA pulse metric
+        l_ra = copy.deepcopy(GLOBAL_LABELS)
+        l_ra.update(makeLabels(data, ["RADirection", "RALimited"]))
+        createEventMetrics(data, l_ra, ["RADuration"])
+
+        # DEC pulse metric
+        l_dec = copy.deepcopy(GLOBAL_LABELS)
+        l_dec.update(makeLabels(data, ["DECDirection", "DecLimited"]))
+        createEventMetrics(data, l_dec, ["DECDuration"])
+
+        # collect data for RMS
+        rms_keys = ["DECDistanceRaw", "DECDistanceGuide", "RADistanceRaw", "RADistanceGuide"]
+        calculate_rms = True
+        for key in rms_keys:
+            if key not in data:
+                calculate_rms = False
+                continue
+            if key not in PHD_RMS_DATA:
+                PHD_RMS_DATA[key] = []
+            if len(PHD_RMS_DATA[key]) >= PHD_RMS_SAMPLES:
+                PHD_RMS_DATA[key].pop(0)
+            else:
+                calculate_rms = False
+            PHD_RMS_DATA[key].append(data[key])
+
+
+        if calculate_rms == True:
+            # fabricate data for "total" as hypotenuse of each DEC/RA {Raw, Guide} tuple
+            PHD_RMS_DATA["TotalDistanceRaw"] = []
+            PHD_RMS_DATA["TotalDistanceGuide"] = []
+            for i in range(0, PHD_RMS_SAMPLES):
+                PHD_RMS_DATA["TotalDistanceRaw"].append(
+                    math.sqrt(
+                        math.pow(PHD_RMS_DATA["DECDistanceRaw"][i],2)
+                        + math.pow(PHD_RMS_DATA["RADistanceRaw"][i],2)
+                    )
+                )
+                PHD_RMS_DATA["TotalDistanceGuide"].append(
+                    math.sqrt(
+                        math.pow(PHD_RMS_DATA["DECDistanceGuide"][i],2)
+                        + math.pow(PHD_RMS_DATA["RADistanceGuide"][i],2)
+                    )
+                )
+
+            # do the math for RMS and export metrics
+            rms_square_of_sums = {}
+            rms_px = {}
+            # for each key...
+            for key in PHD_RMS_DATA.keys():
+                if key not in rms_square_of_sums:
+                    rms_square_of_sums[key] = 0
+                # do the sum of squares bit
+                for value in PHD_RMS_DATA[key]:
+                    rms_square_of_sums[key] += math.pow(value, 2)
+                # then divide and sqrt
+                rms_px[key] = math.sqrt(rms_square_of_sums[key] / PHD_RMS_SAMPLES)
+                utility.set("phd2_rms", rms_px[key], {"source": key, "scale": "px"})
+            
+                # calculate for arcsec if we have pixel scale
+                if PIXEL_SCALE > 0:
+                    rms_arcsec = rms_px[key] * PIXEL_SCALE
+                    utility.set("phd2_rms", rms_arcsec, {"source": key, "scale": "arcsec"})
+                
+
+
+    elif event == "GuidingDithered":
+        createEventMetrics(data, GLOBAL_LABELS, ["dx", "dy"])
+
+    utility.inc(f"phd2_{event}", GLOBAL_LABELS)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Export PHD2 events as prometheus metrics.")
+    parser.add_argument("--phd2port", type=int, help="the port PHD2 is exposing events on, default: 4400")
+    parser.add_argument("--phd2host", type=str, help="the host PHD2 is exposing events on, default: 127.0.0.1")
+    parser.add_argument("--port", type=int, help="the port to export metrics on, default: 9753")
+    parser.add_argument("--rms_samples", type=int, help="number of samples used to calculate RMS, default: 10")
+
+    # treat args parsed as a dictionary
+    args = vars(parser.parse_args())
+
+    phd2port = 4400
+    if "phd2port" in args and args["phd2port"]:
+        phd2port = args["phd2port"]
+    phd2host = "127.0.0.1"
+    if "phd2host" in args and args["phd2host"]:
+        phd2host = args["phd2host"]
+    port = 9753
+    if "port" in args and args["port"]:
+        port = args["port"]
+    rms_samples = 10
+    if "rms_samples" in args and args["rms_samples"]:
+        rms_samples = args["rms_samples"]
+    
+    PHD_RMS_SAMPLES = rms_samples
+
+    # Start up the server to expose the metrics.
+    utility.metrics(port)
+
+    print_connect_error = True
+
+    while True:
+        try:
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+                s.connect((phd2host,phd2port))
+                print_connect_error = True
+                s.setblocking(0)
+                s.settimeout(2.0)
+                while True:
+                    try:
+                        raw = s.recv(1024)
+                        if len(raw) == 0:
+                            # empty response without a timeout, server is down
+                            # break the loop, which triggers a reconnect
+                            break
+                    except socket.timeout:
+                        # this is fine, just nothing to read.  PHD2 idle.
+                        continue
+                    except Exception as e:
+                        # non-timeout error, this is bad.  break the loop, which will trigger reconnect
+                        print(e)
+                        break
+                    lines = raw.decode()
+                    for line in lines.split('\r\n'):
+                        if len(line) > 0:
+                            data = json.loads(line)
+                            if "Event" in data:
+                                handleEvent(s, data)
+                            elif "jsonrpc" in data and "id" in data:
+                                id = data["id"]
+                                JRPC_CALLBACKS[id](data)
+        except socket.error as e:
+            # 10061 is connection refused.  don't log these, it's spammy
+            if e.errno == 10061:
+                if print_connect_error == True:
+                    print("Failed to connect to PHD2.  Server is down or unreachable.  Retrying silently...")
+                    print_connect_error = False
+            else:
+                print(e)
+            pass
+        except Exception as e:
+            print(e)
+            pass
+    
+        # something went wrong.  try again in a little bit...
+        time.sleep(2)
+
