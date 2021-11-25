1e6771241d0cf6e8b2343bfec0ae1a190c6b7ff1
Naveen Malik <nmalik@redhat.com>
2021-11-24 22:11:09 -0500

    Tweaking as I try it out..
    Added some count labels. Added recording rules. Noted I need to not use "instance" as a label.  Wipe RMS if settling.  Don't calculate RMS while settling.

--- src/phd2-exporter.py
+++ src/phd2-exporter.py
@@ -12,14 +12,15 @@ with httpimport.github_repo('jewzaam', 'metrics-utility', 'utility', 'main'):
     import utility
 
 PHD_STATE = ""
+PHD_SETTLING = False
 PHD_RMS_SAMPLES = 15
 PHD_RMS_DATA = {}
 PIXEL_SCALE = 0
 JRPC_CALLBACKS = {}
 
-GLOBAL_LABELS = {}
+GLOBAL_LABELS = None
 
-DEBUG = True
+DEBUG = False
 
 def debug(message):
     # simply so I can easily control debug messages
@@ -30,6 +31,9 @@ def callback_requestPixelScale(data):
     global GLOBAL_LABELS, PIXEL_SCALE
     PIXEL_SCALE = data["result"]
     utility.set("phd2_pixel_scale", PIXEL_SCALE, GLOBAL_LABELS)
+    if "id" in data:
+        # unregister the callback, work is done
+        JRPC_CALLBACKS.pop(data["id"])
 
 def requestPixelScale(s):
     global JRPC_CALLBACKS
@@ -64,7 +68,7 @@ def createEventMetrics(data, labels, metric_keys):
             utility.set(f"phd2_{data['Event']}_{metric_key}", data[metric_key], labels)
 
 def handleEvent(s, data):
-    global PHD_STATE, PHD_RMS_SAMPLES, GLOBAL_LABELS, PHD_RMS_DATA
+    global PHD_STATE, PHD_RMS_SAMPLES, GLOBAL_LABELS, PHD_RMS_DATA, PHD_SETTLING
 
     if 'Event' not in data:
         return
@@ -80,8 +84,6 @@ def handleEvent(s, data):
         "LostLock",     # PHD is guiding, but the frame was dropped
         "Paused",       # PHD is paused
         "Looping",      # PHD is looping exposures
-        # additional states
-        "Settling",     # PHD is settling
     ]
     # use global PHD_STATE to get an updated state, which is then updated at the end of the function
     
@@ -89,17 +91,19 @@ def handleEvent(s, data):
         # initialze global labels
         GLOBAL_LABELS = {
             "host": data["Host"].lower(),
-            "instance": data["Inst"],
+            "inst": data["Inst"],
         }
 
     # update state based on event
     if event in ["LoopingExposuresStopped"]:
         PHD_STATE="Stopped"
+        # reset RMS data
+        PHD_RMS_DATA = {}
     elif event in ["LockPositionSet"]:
         PHD_STATE="Selected"
     elif event in ["StartCalibration", "Calibrating"]:
         PHD_STATE="Calibrating"
-    elif event in ["GuideStep", "SettleDone"]:
+    elif event in ["GuideStep"]:
         PHD_STATE="Guiding"
     elif event in ["StarLost"]:
         PHD_STATE="LostLock"
@@ -107,20 +111,27 @@ def handleEvent(s, data):
         PHD_STATE="Paused"
     elif event in ["LoopingExposures"]:
         PHD_STATE="Looping"
-    elif event in ["SettleBegin", "Settling"]:
-        PHD_STATE="Settling"
     elif event == "AppState" and "State" in data:
         PHD_STATE = data["State"]
+        PHD_SETTLING=False
+    elif event in ["SettleBegin", "Settling"]:
+        PHD_SETTLING=True
+    elif event in ["SettleDone"]:
+        PHD_SETTLING=False
 
     for state in app_states:
-        value = 0
-        if state == PHD_STATE:
-            value = 1
+        value = int(PHD_STATE == state)
         l = copy.deepcopy(GLOBAL_LABELS)
         l.update({'status': state})
         utility.set("phd2_status", value, l)
+    
+    # set settling status, which can be true while other states are true
+    value = (PHD_SETTLING == True)
+    l = copy.deepcopy(GLOBAL_LABELS)
+    l.update({'status': "Settling"})
+    utility.set("phd2_status", value, l)
 
-    # did equipment change? get pixel scale!
+    # did equipment change? get pixel scale! uses callback so reading is handled in main loop
     if event in ["AppState", "ConfigurationChange"]:
         requestPixelScale(s)
 
@@ -151,66 +162,60 @@ def handleEvent(s, data):
         l_dec.update(makeLabels(data, ["DECDirection", "DecLimited"]))
         createEventMetrics(data, l_dec, ["DECDuration"])
 
-        # collect data for RMS
-        rms_keys = ["DECDistanceRaw", "DECDistanceGuide", "RADistanceRaw", "RADistanceGuide"]
-        calculate_rms = True
-        for key in rms_keys:
-            if key not in data:
-                calculate_rms = False
-                continue
-            if key not in PHD_RMS_DATA:
-                PHD_RMS_DATA[key] = []
-            if len(PHD_RMS_DATA[key]) >= PHD_RMS_SAMPLES:
-                PHD_RMS_DATA[key].pop(0)
-            else:
-                calculate_rms = False
-            PHD_RMS_DATA[key].append(data[key])
+        # skip RMS if settling
+        if PHD_SETTLING == False:
+            # collect data for RMS
+            rms_keys = ["DECDistanceRaw", "DECDistanceGuide", "RADistanceRaw", "RADistanceGuide"]
+            calculate_rms = True
+            for key in rms_keys:
+                if key not in data:
+                    calculate_rms = False
+                    continue
+                if key not in PHD_RMS_DATA:
+                    PHD_RMS_DATA[key] = []
+                if len(PHD_RMS_DATA[key]) >= PHD_RMS_SAMPLES:
+                    PHD_RMS_DATA[key].pop(0)
+                else:
+                    calculate_rms = False
+                PHD_RMS_DATA[key].append(data[key])
 
-
-        if calculate_rms == True:
-            # fabricate data for "total" as hypotenuse of each DEC/RA {Raw, Guide} tuple
-            PHD_RMS_DATA["TotalDistanceRaw"] = []
-            PHD_RMS_DATA["TotalDistanceGuide"] = []
-            for i in range(0, PHD_RMS_SAMPLES):
-                PHD_RMS_DATA["TotalDistanceRaw"].append(
-                    math.sqrt(
-                        math.pow(PHD_RMS_DATA["DECDistanceRaw"][i],2)
-                        + math.pow(PHD_RMS_DATA["RADistanceRaw"][i],2)
+            if calculate_rms == True:
+                # fabricate data for "total" as hypotenuse of each DEC/RA {Raw, Guide} tuple
+                PHD_RMS_DATA["TotalDistanceRaw"] = []
+                PHD_RMS_DATA["TotalDistanceGuide"] = []
+                for i in range(0, PHD_RMS_SAMPLES):
+                    PHD_RMS_DATA["TotalDistanceRaw"].append(
+                        math.sqrt(
+                            math.pow(PHD_RMS_DATA["DECDistanceRaw"][i],2)
+                            + math.pow(PHD_RMS_DATA["RADistanceRaw"][i],2)
+                        )
                     )
-                )
-                PHD_RMS_DATA["TotalDistanceGuide"].append(
-                    math.sqrt(
-                        math.pow(PHD_RMS_DATA["DECDistanceGuide"][i],2)
-                        + math.pow(PHD_RMS_DATA["RADistanceGuide"][i],2)
+                    PHD_RMS_DATA["TotalDistanceGuide"].append(
+                        math.sqrt(
+                            math.pow(PHD_RMS_DATA["DECDistanceGuide"][i],2)
+                            + math.pow(PHD_RMS_DATA["RADistanceGuide"][i],2)
+                        )
                     )
-                )
 
-            # do the math for RMS and export metrics
-            rms_square_of_sums = {}
-            rms_px = {}
-            # for each key...
-            for key in PHD_RMS_DATA.keys():
-                if key not in rms_square_of_sums:
-                    rms_square_of_sums[key] = 0
-                # do the sum of squares bit
-                for value in PHD_RMS_DATA[key]:
-                    rms_square_of_sums[key] += math.pow(value, 2)
-                # then divide and sqrt
-                rms_px[key] = math.sqrt(rms_square_of_sums[key] / PHD_RMS_SAMPLES)
-                utility.set("phd2_rms", rms_px[key], {"source": key, "scale": "px"})
-            
-                # calculate for arcsec if we have pixel scale
-                if PIXEL_SCALE > 0:
-                    rms_arcsec = rms_px[key] * PIXEL_SCALE
-                    utility.set("phd2_rms", rms_arcsec, {"source": key, "scale": "arcsec"})
+                # do the math for RMS and export metrics
+                for key in PHD_RMS_DATA.keys():
+                    rms_sum_of_squares = 0
+                    # do the sum of squares bit
+                    for value in PHD_RMS_DATA[key]:
+                        rms_sum_of_squares += math.pow(value, 2)
+                    # then divide and sqrt
+                    rms_px = math.sqrt(rms_sum_of_squares / PHD_RMS_SAMPLES)
+                    utility.set("phd2_rms", rms_px, {"source": key, "scale": "px"})
                 
-
+                    # calculate for arcsec if we have pixel scale
+                    if PIXEL_SCALE > 0:
+                        rms_arcsec = rms_px * PIXEL_SCALE
+                        utility.set("phd2_rms", rms_arcsec, {"source": key, "scale": "arcsec"})
 
     elif event == "GuidingDithered":
         createEventMetrics(data, GLOBAL_LABELS, ["dx", "dy"])
 
     utility.inc(f"phd2_{event}", GLOBAL_LABELS)
-
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Export PHD2 events as prometheus metrics.")
@@ -256,11 +261,18 @@ if __name__ == '__main__':
                             # empty response without a timeout, server is down
                             # break the loop, which triggers a reconnect
                             break
+                        # keep a count (inc / total) for everything
+                        if GLOBAL_LABELS is not None:
+                            utility.inc("phd2", GLOBAL_LABELS)
                     except socket.timeout:
-                        # this is fine, just nothing to read.  PHD2 idle.
+                        # this is fine, just nothing to read.  PHD2 idle.  No metric so it's not confused as an error
+                        # keep a count (inc / total) for everything
+                        if GLOBAL_LABELS is not None:
+                            utility.inc("phd2", GLOBAL_LABELS)
                         continue
                     except Exception as e:
                         # non-timeout error, this is bad.  break the loop, which will trigger reconnect
+                        utility.inc("phd2_error", {"type": type(e).__name__})
                         print(e)
                         break
                     lines = raw.decode()
@@ -273,6 +285,7 @@ if __name__ == '__main__':
                                 id = data["id"]
                                 JRPC_CALLBACKS[id](data)
         except socket.error as e:
+            utility.inc("phd2_error", {"type": type(e).__name__})
             # 10061 is connection refused.  don't log these, it's spammy
             if e.errno == 10061:
                 if print_connect_error == True:
@@ -282,6 +295,7 @@ if __name__ == '__main__':
                 print(e)
             pass
         except Exception as e:
+            utility.inc("phd2_error", {"type": type(e).__name__})
             print(e)
             pass
     
